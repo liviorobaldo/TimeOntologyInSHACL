fb6c262de38faa8a2d7373dc203fb76a26624a9e
liviorobaldo <40432737+liviorobaldo@users.noreply.github.com>
2025-10-17 11:59:41 +0100

    Delete run.bat

--- run.bat
+++
@@ -1,6 +0,0 @@
-REM comment/uncomment below what you want to execute.
-
-java -cp .;./lib/spark/*;./lib/* -Dfile.encoding=utf-8 InferAndValidateThroughSHACL
-REM java -cp .;./lib/spark/*;./lib/* -Dfile.encoding=utf-8 InferThroughOWL
-REM java -cp .;./lib/spark/*;./lib/* -Dfile.encoding=utf-8 SyntheticDatasetGenerators/generateAllensCompositionTableOn2ProperIntervals
-REM java -cp .;./lib/spark/*;./lib/* -Dfile.encoding=utf-8 SyntheticDatasetGenerators/generateAllensTemporalRelationsOn2ProperIntervals
