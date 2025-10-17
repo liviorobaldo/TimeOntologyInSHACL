e81209d0994af1f8307fb7e05ea257edd80be932
liviorobaldo <40432737+liviorobaldo@users.noreply.github.com>
2025-10-17 11:59:33 +0100

    Delete compile.bat

--- compile.bat
+++
@@ -1,4 +0,0 @@
-javac -cp .;./lib/spark/*;./lib/* InferAndValidateThroughSHACL.java
-javac -cp .;./lib/spark/*;./lib/* InferThroughOWL.java
-javac -cp .;./lib/spark/*;./lib/* SyntheticDatasetGenerators/generateAllensCompositionTableOn2ProperIntervals.java
-javac -cp .;./lib/spark/*;./lib/* SyntheticDatasetGenerators/generateAllensTemporalRelationsOn2ProperIntervals.java
