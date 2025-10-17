414eee54747296d056b255eba68b9467e7b705d3
liviorobaldo <40432737+liviorobaldo@users.noreply.github.com>
2025-10-17 12:23:51 +0100

    Delete run.bat

--- run.bat
+++
@@ -1,4 +0,0 @@
-@REM comment/uncomment below what you want to execute.
-@REM run on OpenJDK8, also available at https://www.openlogic.com/openjdk-downloads
-
-OpenJDK8\bin\java -cp .;.\lib\* -Dfile.encoding=UTF-8 InferAndValidateThroughSHACL ./Examples/Example14.rdf
